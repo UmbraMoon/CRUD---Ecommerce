b083511b4947a494efaff8354fff80ddd06431ef
Guilherme <guilherme.devtechno@gmail.com>
2024-10-31 10:32:25 -0300

    Add files via upload

---
+++ ClienteADO.java
@@ -0,0 +1,34 @@
+public class ClienteADO {
+
+    // Método para INSERT um novo cliente. o método insere novos clientes e suas informações na tabela.
+
+    public void insertCliente () {
+
+    }
+    //Fim método INSERT
+
+
+    // Método para SELECT todos os clientes. O método mostra os clientes já cadastrados e suas informações.
+
+    public void selectCliente (){
+
+    }
+    //Fim método SELECT
+
+
+    // Método para atualizar um cliente existente, no qual altera os dados do cliente. (Deve dar update em todas as informações.
+    public void updateCliente(){
+
+    }
+    //Fim método DELETE
+
+
+    // Método para deletar um cliente. o método deleta um cliente JÁ cadastrado na tebela
+    public void deleteCliente(){
+
+    }
+    //Fim método DELETE
+
+
+    //Fim código ClienteADO;
+}
